a59b7fbe7fbc498e08e4fb6f2da8bbd303de0ebc
Hunkar lule <L.hunkar@gmail.com>
2024-04-10 09:09:08 -0700

    how # include works.

--- 02-Jan11/prg.cpp
+++ 02-Jan11/prg.cpp
@@ -1,9 +1,6 @@
-#include <iostream>
 
-using namespace std;
-
-int main() {
-	cout << "Hello world!" << endl;
-	cout << "How are you today?" << endl;
-	return 0;
-}
+// how # inlcude works?
+// # includes means basically "copy-paste" the content of the file.
+// after prorpocessor makes the copy-paste, the compiler will start the compilation.
+#include "heehaw.hoohoo" // pre-processor will copy paste content of  file heehaw.hoohoo insterad of this line.
+#include "hoohoo.txt" // pre-processor will copy paste content of  file hoohoo.txt insterad of this line.
